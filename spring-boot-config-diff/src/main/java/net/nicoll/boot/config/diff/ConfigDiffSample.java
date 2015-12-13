9483ccf578265bf969ce0d9fbb9463c750a7303c
Stephane Nicoll <snicoll@pivotal.io>
2015-12-13 10:03:33 +0100

    Update Spring Boot versions

--- spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
+++ spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
@@ -26,7 +26,7 @@ public class ConfigDiffSample {
 
 	public static void main(String[] args) throws Exception {
 		ConfigDiffResult configDiffResult = new ConfigDiffGenerator(AetherDependencyResolver.withAllRepositories())
-				.generateDiff("1.3.0.RC1", "1.3.0.BUILD-SNAPSHOT");
+				.generateDiff("1.2.7.RELEASE", "1.3.0.RELEASE");
 
 		ConfigDiffFormatter formatter = new AsciiDocConfigDiffFormatter();
 
