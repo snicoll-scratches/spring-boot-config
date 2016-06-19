f5350136d1c0bf6cbb2e5db665dacfdfa5fad740
Stephane Nicoll <snicoll@pivotal.io>
2016-06-19 13:22:11 +0200

    Update for 1.4.0.M3

--- spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
+++ spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
@@ -26,7 +26,7 @@ public class ConfigDiffSample {
 
 	public static void main(String[] args) throws Exception {
 		ConfigDiffResult configDiffResult = new ConfigDiffGenerator(AetherDependencyResolver.withAllRepositories())
-				.generateDiff("1.4.0.M2", "1.4.0.BUILD-SNAPSHOT");
+				.generateDiff("1.4.0.M3", "1.4.0.BUILD-SNAPSHOT");
 
 		ConfigDiffFormatter formatter = new AsciiDocConfigDiffFormatter();
 
