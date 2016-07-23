f3448b7a88097992ad7409150db96240dd35a379
Stephane Nicoll <snicoll@pivotal.io>
2016-07-23 08:46:49 +0200

    Prepare changelog for 1.4.0.RELEASE

--- spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
+++ spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
@@ -26,7 +26,7 @@ public class ConfigDiffSample {
 
 	public static void main(String[] args) throws Exception {
 		ConfigDiffResult configDiffResult = new ConfigDiffGenerator(AetherDependencyResolver.withAllRepositories())
-				.generateDiff("1.4.0.RC1", "1.4.0.BUILD-SNAPSHOT");
+				.generateDiff("1.3.6.RELEASE", "1.4.0.BUILD-SNAPSHOT");
 
 		ConfigDiffFormatter formatter = new AsciiDocConfigDiffFormatter();
 
