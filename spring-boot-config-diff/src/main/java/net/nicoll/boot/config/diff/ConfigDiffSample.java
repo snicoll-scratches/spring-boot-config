abe525a7388de21e9568d0eed7a876e2f6e10c35
Stephane Nicoll <snicoll@pivotal.io>
2016-09-18 08:26:36 +0200

    Upgrade to Spring Boot 1.4.1 snapshot

--- spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
+++ spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
@@ -26,7 +26,7 @@ public class ConfigDiffSample {
 
 	public static void main(String[] args) throws Exception {
 		ConfigDiffResult configDiffResult = new ConfigDiffGenerator(AetherDependencyResolver.withAllRepositories())
-				.generateDiff("1.3.6.RELEASE", "1.4.0.RELEASE");
+				.generateDiff("1.4.0.RELEASE", "1.4.1.BUILD-SNAPSHOT");
 
 		ConfigDiffFormatter formatter = new AsciiDocConfigDiffFormatter();
 
