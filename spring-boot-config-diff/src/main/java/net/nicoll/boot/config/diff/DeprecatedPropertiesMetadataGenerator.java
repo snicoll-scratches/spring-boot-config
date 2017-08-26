d41ab592c6252dbedb68025016db20273db7b7cd
Stephane Nicoll <snicoll@pivotal.io>
2017-08-26 08:58:28 +0200

    Split error metadata in modules
    This commit makes sure to detect to which module a metadata item belongs
    to and split the output per module.
    
    Also, if the property was deprecated in the previous release and removed
    in the current, no metadata is generated as users have already been
    given a fair warning.

--- spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/DeprecatedPropertiesMetadataGenerator.java
+++ spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/DeprecatedPropertiesMetadataGenerator.java
@@ -3,13 +3,18 @@ package net.nicoll.boot.config.diff;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.nicoll.boot.config.loader.AetherDependencyResolver;
+import net.nicoll.boot.config.loader.ConfigurationMetadataLoader;
 
 import org.springframework.boot.configurationmetadata.ConfigurationMetadataProperty;
+import org.springframework.boot.configurationmetadata.ConfigurationMetadataRepository;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 /**
  * Generates a JSON structure for all keys that were removed between two versions. This
@@ -24,17 +29,32 @@ import org.springframework.boot.configurationmetadata.ConfigurationMetadataPrope
 public class DeprecatedPropertiesMetadataGenerator {
 
 	public static void main(String[] args) throws Exception {
-		ConfigDiffResult configDiffResult = new ConfigDiffGenerator(
-				AetherDependencyResolver.withAllRepositories()).generateDiff(
-				"1.5.4.BUILD-SNAPSHOT", "2.0.0.BUILD-SNAPSHOT");
+		String from = "1.5.6.RELEASE";
+		String to = "2.0.0.BUILD-SNAPSHOT";
 
-		DeprecatedPropertyJsonFormatter formatter = new DeprecatedPropertyJsonFormatter();
+		AetherDependencyResolver dependencyResolver = AetherDependencyResolver
+				.withAllRepositories();
+		ConfigDiffResult configDiffResult = new ConfigDiffGenerator(dependencyResolver)
+				.generateDiff(from, to);
+		ConfigurationMetadataLoader loader = new ConfigurationMetadataLoader(
+				dependencyResolver);
+		ConfigurationMetadataRepository repository = loader.loadRepository(to);
+
+		DeprecatedPropertyJsonFormatter formatter = new DeprecatedPropertyJsonFormatter(
+				repository);
 
 		System.out.println(formatter.formatDiff(configDiffResult));
 	}
 
 
 	private static class DeprecatedPropertyJsonFormatter implements ConfigDiffFormatter {
+
+		private final ConfigurationMetadataRepository repository;
+
+		private DeprecatedPropertyJsonFormatter(
+				ConfigurationMetadataRepository repository) {
+			this.repository = repository;
+		}
 
 		@Override
 		public String formatDiff(ConfigDiffResult result) throws IOException {
@@ -43,12 +63,67 @@ public class DeprecatedPropertiesMetadataGenerator {
 
 
 			List<DeprecatedItem> items = deleted.stream()
-					.map(e -> new DeprecatedItem(e.getLeft(), null))
+					.filter(e -> !e.getLeft().isDeprecated())
+					.map(e -> new DeprecatedItem(e.getLeft(),
+							detectReplacement(e.getLeft()), detectReason(e.getLeft())))
 					.collect(Collectors.toList());
-			items.sort(Comparator.comparing(DeprecatedItem::getName));
 
+			MultiValueMap<String, DeprecatedItem> groups = new LinkedMultiValueMap<>();
+			items.forEach(item -> groups.add(detectGroup(item.getName()), item));
+
+
+			StringBuilder sb = new StringBuilder();
 			ObjectMapper mapper = new ObjectMapper();
-			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(items);
+			for (Map.Entry<String, List<DeprecatedItem>> e : groups.entrySet()) {
+				List<DeprecatedItem> groupItems = e.getValue();
+				groupItems.sort(Comparator.comparing(DeprecatedItem::getName));
+				sb.append(String.format("Add to `%s`%n%n", e.getKey()));
+				sb.append(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(groupItems));
+				sb.append(String.format("%n"));
+			}
+			return sb.toString();
+		}
+
+		private String detectReplacement(ConfigurationMetadataProperty property) {
+			String[] parts = property.getId().split("\\.");
+			if (parts.length == 2) {
+				List<ConfigurationMetadataProperty> properties = extractSimilarKey(property.getId());
+				if (properties.size() == 1) {
+					return properties.get(0).getId();
+				}
+			}
+			if (parts.length > 2) {
+				String attempt = parts[parts.length - 2] + "." + parts[parts.length - 1];
+				List<ConfigurationMetadataProperty> properties = extractSimilarKey(attempt);
+				if (properties.size() == 1) {
+					return properties.get(0).getId();
+				}
+			}
+			List<ConfigurationMetadataProperty> properties = extractSimilarKey(parts[parts.length - 1]);
+			if (properties.size() == 1) {
+				return properties.get(0).getId();
+			}
+			return null;
+		}
+
+		private String detectReason(ConfigurationMetadataProperty property) {
+			String id = property.getId();
+			// TODO: add reason for well knonw keys
+
+			return null;
+		}
+
+		private List<ConfigurationMetadataProperty> extractSimilarKey(String part) {
+			return this.repository.getAllProperties().values().stream()
+					.filter(p -> p.getId().endsWith(part))
+					.collect(Collectors.toList());
+		}
+
+		private String detectGroup(String name) {
+			if (name.startsWith("endpoints") || name.startsWith("management")) {
+				return "spring-boot-actuator";
+			}
+			return "spring-boot-autoconfigure";
 		}
 	}
 
@@ -66,12 +141,12 @@ public class DeprecatedPropertiesMetadataGenerator {
 		private final Deprecation deprecation;
 
 		public DeprecatedItem(ConfigurationMetadataProperty property,
-				String replacement) {
+				String replacement, String reason) {
 			this.name = property.getId();
 			this.type = property.getType();
 			this.description = property.getDescription();
 			this.defaultValue = property.getDefaultValue();
-			this.deprecation = new Deprecation(replacement);
+			this.deprecation = new Deprecation(replacement, reason);
 		}
 
 		public String getName() {
@@ -90,11 +165,6 @@ public class DeprecatedPropertiesMetadataGenerator {
 			return this.defaultValue;
 		}
 
-		public boolean isDeprecated() {
-			return true;
-		}
-
-
 		public Deprecation getDeprecation() {
 			return this.deprecation;
 		}
@@ -103,8 +173,11 @@ public class DeprecatedPropertiesMetadataGenerator {
 		public static class Deprecation {
 			private final String replacement;
 
-			public Deprecation(String replacement) {
+			private final String reason;
+
+			public Deprecation(String replacement, String reason) {
 				this.replacement = replacement;
+				this.reason = reason;
 			}
 
 			public String getLevel() {
@@ -113,6 +186,10 @@ public class DeprecatedPropertiesMetadataGenerator {
 
 			public String getReplacement() {
 				return this.replacement;
+			}
+
+			public String getReason() {
+				return this.reason;
 			}
 		}
 	}
