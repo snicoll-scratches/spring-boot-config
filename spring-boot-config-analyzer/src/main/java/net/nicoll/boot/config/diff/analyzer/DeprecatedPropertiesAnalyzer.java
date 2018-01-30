9e4fad65a66c0cd25fd599aa7383efc93fd4362a
Stephane Nicoll <snicoll@pivotal.io>
2018-01-30 16:51:35 +0100

    Add an analyzer of deprecated properties
    This commits adds an analyzer that validates all properties that are
    flagged as deprecated. If a replacement is provided and that replacement
    does not exist, the property is flagged in the report.

---
+++ spring-boot-config-analyzer/src/main/java/net/nicoll/boot/config/diff/analyzer/DeprecatedPropertiesAnalyzer.java
@@ -0,0 +1,137 @@
+package net.nicoll.boot.config.diff.analyzer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import net.nicoll.boot.config.diff.ConfigDiffEntry;
+import net.nicoll.boot.config.diff.ConfigDiffGenerator;
+import net.nicoll.boot.config.diff.ConfigDiffResult;
+import net.nicoll.boot.config.diff.ConfigDiffType;
+import net.nicoll.boot.config.diff.SentenceExtractor;
+import net.nicoll.boot.config.loader.AetherDependencyResolver;
+import net.nicoll.boot.config.loader.ConfigurationMetadataLoader;
+
+import org.springframework.boot.configurationmetadata.ConfigurationMetadataProperty;
+import org.springframework.boot.configurationmetadata.ConfigurationMetadataRepository;
+import org.springframework.boot.configurationmetadata.Deprecation;
+
+/**
+ * Analyze deprecated configuration, making sure that the replacement refers to
+ * an existing property.
+ *
+ * @author Stephane Nicoll
+ */
+public class DeprecatedPropertiesAnalyzer {
+
+	public static void main(String[] args) throws Exception {
+		String from = "1.5.9.RELEASE";
+		String to = "2.0.0.BUILD-SNAPSHOT";
+
+		AetherDependencyResolver dependencyResolver = AetherDependencyResolver
+				.withAllRepositories();
+		ConfigDiffGenerator configDiffGenerator = new ConfigDiffGenerator(
+				dependencyResolver);
+		ConfigurationMetadataLoader loader =
+				new ConfigurationMetadataLoader(dependencyResolver);
+		ConfigDiffResult diff = configDiffGenerator.generateDiff(from, to);
+		DeprecatedPropertiesReporter reporter = new DeprecatedPropertiesReporter(diff,
+				loader.loadRepository(to));
+		System.out.println(reporter.getReport());
+	}
+
+
+	private static class DeprecatedPropertiesReporter {
+
+		private final ConfigDiffResult diff;
+		private final ConfigurationMetadataRepository repository;
+
+		DeprecatedPropertiesReporter(ConfigDiffResult diff,
+				ConfigurationMetadataRepository repository) {
+			this.diff = diff;
+			this.repository = repository;
+		}
+
+		public String getReport() {
+			List<String> valid = new ArrayList<>();
+			List<String> invalid = new ArrayList<>();
+			List<String> errors = new ArrayList<>();
+			List<ConfigDiffEntry<ConfigurationMetadataProperty>> properties = diff.getPropertiesDiffFor(
+					ConfigDiffType.DEPRECATE);
+
+			properties.stream().filter(this::hasReplacement).forEach(e -> {
+				ConfigurationMetadataProperty current = e.getRight();
+				String candidate = current.getDeprecation().getReplacement();
+				ConfigurationMetadataProperty replacement = getReplacementMetadata(candidate);
+				if (replacement != null) {
+					valid.add(current.getId() + " replaced by " + replacement.getId());
+				}
+				else {
+					invalid.add(current.getId() + " with invalid replacement " + candidate);
+				}
+			});
+
+			properties.stream().filter(e -> !hasReplacement(e)).forEach(e -> {
+				ConfigurationMetadataProperty current = e.getRight();
+				errors.add(String.format("%s - %s", current.getId(),
+						current.getDeprecation().getReason() != null
+								? SentenceExtractor.getFirstSentence(current.getDeprecation().getReason())
+								: "none"));
+			});
+			StringBuilder message = new StringBuilder();
+			message.append(String.format("Found %d deprecated properties%n", properties.size()));
+			message.append(String.format("\t%d have a valid replacement%n", valid.size()));
+			message.append(String.format("\t%d have an invalid replacement and must be fixed%n", invalid.size()));
+			message.append(String.format("\t%d have no replacement%n", errors.size()));
+			if (!invalid.isEmpty()) {
+				message.append(String.format("%n%n"));
+				message.append(String.format("Invalid replacements%n"));
+				Collections.sort(invalid);
+				invalid.forEach(e -> message.append(String.format("\t%s%n", e)));
+			}
+			if (!errors.isEmpty()) {
+				message.append(String.format("%n%n"));
+				message.append(String.format("Entries with no replacement%n"));
+				Collections.sort(errors);
+				errors.forEach(e -> message.append(String.format("\t%s%n", e)));
+			}
+			if (!valid.isEmpty()) {
+				message.append(String.format("%n%n"));
+				message.append(String.format("Entries with valid replacement%n"));
+				Collections.sort(valid);
+				valid.forEach(e -> message.append(String.format("\t%s%n", e)));
+			}
+
+			return message.toString();
+		}
+
+		private ConfigurationMetadataProperty getReplacementMetadata(String candidate) {
+			ConfigurationMetadataProperty replacement = this.repository.getAllProperties().get(
+					candidate);
+			if (replacement != null) {
+				return replacement;
+			}
+			int lastDot = candidate.lastIndexOf('.');
+			if (lastDot != -1) {
+				ConfigurationMetadataProperty property = this.repository.getAllProperties()
+						.get(candidate.substring(0, lastDot));
+				if (property != null) {
+					String type = property.getType();
+					if (type != null && type.startsWith(Map.class.getName())) {
+						return property;
+					}
+				}
+			}
+			return null;
+		}
+
+		private boolean hasReplacement(ConfigDiffEntry<ConfigurationMetadataProperty> e) {
+			return e.getRight().getDeprecation() != null
+					&& e.getRight().getDeprecation().getReplacement() != null;
+		}
+
+	}
+
+}
