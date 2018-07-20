e931c68b5171ce7511e00a14e1d4c819c20d82fd
Stephane Nicoll <snicoll@pivotal.io>
2018-07-20 15:14:51 +0200

    Switch to latest snap

--- spring-boot-config-analyzer/src/main/java/net/nicoll/boot/config/diff/analyzer/DeprecatedPropertiesAnalyzer.java
+++ spring-boot-config-analyzer/src/main/java/net/nicoll/boot/config/diff/analyzer/DeprecatedPropertiesAnalyzer.java
@@ -27,8 +27,8 @@ import org.springframework.boot.configurationmetadata.Deprecation;
 public class DeprecatedPropertiesAnalyzer {
 
 	public static void main(String[] args) throws Exception {
-		String from = "1.5.9.RELEASE";
-		String to = "2.0.0.BUILD-SNAPSHOT";
+		String from = "2.0.3.RELEASE";
+		String to = "2.1.0.BUILD-SNAPSHOT";
 
 		AetherDependencyResolver dependencyResolver = AetherDependencyResolver
 				.withAllRepositories();
