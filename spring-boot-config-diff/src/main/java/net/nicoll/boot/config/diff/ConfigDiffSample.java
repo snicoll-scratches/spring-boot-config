5e86e626b2c39d704b2efb9a40815a6d4a315068
Stephane Nicoll <snicoll@pivotal.io>
2019-05-14 10:17:36 +0200

    Diff against latest state

--- spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
+++ spring-boot-config-diff/src/main/java/net/nicoll/boot/config/diff/ConfigDiffSample.java
@@ -28,7 +28,7 @@ public class ConfigDiffSample {
 		ConfigDiffGenerator configDiffGenerator = new ConfigDiffGenerator(
 				AetherDependencyResolver.withAllRepositories());
 		ConfigDiffResult configDiffResult = configDiffGenerator.generateDiff(
-				"2.2.0.M1", "2.2.0.BUILD-SNAPSHOT");
+				"2.2.0.M2", "2.2.0.BUILD-SNAPSHOT");
 		ConfigDiffFormatter formatter = new AsciiDocConfigDiffFormatter();
 		System.out.println(formatter.formatDiff(configDiffResult));
 	}
